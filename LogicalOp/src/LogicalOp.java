4834781b107a6298bf7773d1d4767e1a01d6c749
aislingmurray <aislingmurray700@gmail.com>
2022-04-01 15:33:54 +0100

    LogicalOperators
    && (AND) || (OR) ! (NOT)

---
+++ LogicalOp/src/LogicalOp.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class LogicalOp
+{
+    public static void main(String[] args)
+    {
+        // logical operators = used to connect two or more expressions
+        // && = (AND) both conditions must be true
+        // || = (OR) either condition must be true
+        // ! = (NOT) reverses the boolean value of condition
+
+        //&& (AND)
+        int temp = 20;
+
+        if (temp > 30)
+        {
+            System.out.println("It is too hot outside");
+        }
+        else if (temp >= 20 && temp <= 30)
+        {
+            System.out.println("It is warm outside");
+        }
+        else
+        {
+            System.out.println("It is cold outside");
+        }
+
+        // || (OR)
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are playing a game! Press q or Q to quit");
+        String response = scanner.next();
+
+        if (response.equals("q") || response.equals("Q"))
+        {
+            System.out.println("You quit the game!");
+        }
+        else
+        {
+            System.out.println("You are still playing the game *pew pew *");
+        }
+
+        // ! (NOT)
+        System.out.println("You are playing a game! Press q or Q to quit");
+        String response2 = scanner.next();
+
+        if (!response.equals("q") && !response.equals("Q"))
+        {
+            System.out.println("You are still playing the game *pew pew *");
+        }
+        else
+        {
+            System.out.println("You quit the game!");
+        }
+    }
+}
