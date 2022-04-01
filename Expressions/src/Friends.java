a6f733ff9c2b18ed1f7e02d31520fa60de71bd71
aislingmurray <aislingmurray700@gmail.com>
2022-04-01 14:38:12 +0100

    Expressions
    Add, subtract, multiply, divide, modulus, increment and decrement.

---
+++ Expressions/src/Friends.java
@@ -0,0 +1,22 @@
+public class Friends
+{
+    public static void main(String[] args)
+    {
+        // esxpression = operands and operators
+        // oppernds = values, variables, numbers, quantity
+        // operators = + - * / %
+        // operators = add, subtract, multiply, divide, modulus
+
+        int friends = 10;
+
+        friends = friends + 1; // add
+        //friends = friends - 2; // subtract
+        //friends = friends * 3; // multiply
+        //friends = friends / 4; // divide, no remainder
+        //friends = friends % 5; // only remainder
+        //friends ++; // plus 1
+        //friends --; // subtract 1
+
+        System.out.println(friends);
+    }
+}
