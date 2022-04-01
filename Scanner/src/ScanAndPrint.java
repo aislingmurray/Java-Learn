7bdfce17e9f24b375c0e5aadc78efebb26c73fe4
aislingmurray <aislingmurray700@gmail.com>
2022-04-01 14:28:04 +0100

    ScanAndPrint2

--- Scanner/src/ScanAndPrint.java
+++ Scanner/src/ScanAndPrint.java
@@ -9,12 +9,13 @@ public class ScanAndPrint
         System.out.println("What is your name: ");
         String name = scanner.nextLine();
 
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
         System.out.println("What is your favourite food?");
         String food = scanner.nextLine();
 
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
-        
         System.out.println("Hello " + name + "!");
         System.out.println("You are " + age + " years old.");
         System.out.println("You like " + food + ".");
